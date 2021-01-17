ee24c9cd8d415cabd79e0c1d8450be859a2a46ca
Noah Syrkis <noah@syrkis.com>
2021-01-17 11:41:27 -0300

    change methyl link to methyl orange.com

--- pages/harmattan.js
+++ pages/harmattan.js
@@ -45,7 +45,7 @@ class HarmattanPage extends Component {
 <div className='summary'>
 				<p className='lorem'>
 					
-						Harmattan is a 2015 photos series shot and edited from the perspective of the main charcater of <a href='/methyl'><i><b>Methyl Orange</b></i></a>.
+						Harmattan is a 2015 photos series shot and edited from the perspective of the main charcater of <a href='https://methylorange.com'><i><b>Methyl Orange</b></i></a>.
 						Presented over the course of nine limited edition prints, the series is available for purchase by contact through <i><b><a href="mailto:contact@syrkis.com?subject=Harmattan inquiry">email</a></b></i>.
 						Proceeds are donated to the <i><a href="https://schistosomiasiscontrolinitiative.org/"><b>SCI Foundation</b></a></i>.
 				</p>
