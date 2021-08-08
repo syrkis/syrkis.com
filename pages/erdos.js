576fd9270a6286600e2977aed63376022a56c661
Noah Syrkis <noah@syrkis.com>
2021-08-08 16:36:27 +0200

    erdos correct text

--- pages/erdos.js
+++ pages/erdos.js
@@ -41,7 +41,7 @@ class ErdosPage extends Component {
 		<div className='summary'>
 						<p className='lorem'>
 							{`
-								The Erdős-Rényi illustrations explore the emergence of a largest component in `}<a href="http://networksciencebook.com/chapter/3" target="_blank"><b><i>{`Erdős-Rényi networks`}</i></b></a>{` seen in this `}<a href="https://youtu.be/YcgdItYO8kM" target='_blank'><b><i>video</i></b></a>{`. Limited edition prints can be bought through`} <i><b><a href="mailto:contact@syrkis.com?subject=Erdos-Renyi Illustrations inquiry">email</a></b></i>{`.
+								The Erdős-Rényi illustrations explore the emergence of a largest component in `}<a href="http://networksciencebook.com/chapter/3" target="_blank"><b><i>{`Erdős-Rényi networks`}</i></b></a>{` as seen in this `}<a href="https://youtu.be/YcgdItYO8kM" target='_blank'><b><i>video</i></b></a>{`. Limited edition prints can be bought through`} <i><b><a href="mailto:contact@syrkis.com?subject=Erdos-Renyi Illustrations inquiry">email</a></b></i>{`.
 								Proceeds go to the development of the
 							`}<i><a href='https://virian.org' target='_blank'><b>Virian Project</b></a></i>.
 						</p>
