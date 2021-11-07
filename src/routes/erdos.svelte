865ee5a9374d0f05042e71a70294ec4b6f258766
Noah Syrkis <noah@syrkis.com>
2021-11-07 23:55:13 +0100

    add erdos link

--- src/routes/erdos.svelte
+++ src/routes/erdos.svelte
@@ -1,7 +1,7 @@
 <script>
     import Gallery from '../comps/gallery.svelte'; 
 
-    let text = `The Erdős-Rényi illustrations explore the emergence of a largest component in Erdős-Rényi networks as seen in this <a href='https://youtu.be/YcgdItYO8kM' target=_blank>video</a>.`
+    let text = `The Erdős-Rényi illustrations explore the emergence of a largest component in <a href="https://en.wikipedia.org/wiki/Erd%C5%91s%E2%80%93R%C3%A9nyi_model" target="_blank">Erdős-Rényi networks</a> as seen in this <a href='https://youtu.be/YcgdItYO8kM' target=_blank>video</a>.`
 
 </script>
 
