3c7dc1028abe27b4e71091df580b777ab77c14e1
Noah Syrkis <noah@syrkis.com>
2021-12-20 12:37:48 +0100

    add milton textures

---
+++ src/routes/milton.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Gallery from '../comps/gallery.svelte'; 
+
+    let text = `The Milton Textures were developed by digitally altering photographs of reflections in water at night.`
+
+</script>
+
+<div>
+    <div>
+        <h1>THE MILTON TEXTURES</h1>
+    </div>
+    <Gallery project='Milton' {text} />
+</div>
