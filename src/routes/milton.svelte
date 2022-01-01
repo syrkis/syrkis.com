edc2b8bf845c6a82e14eb5da1fdcf789a83926a4
Noah Syrkis <noah@syrkis.com>
2022-01-01 08:55:45 +0100

    update v link in gallery

--- src/routes/milton.svelte
+++ src/routes/milton.svelte
@@ -1,7 +1,7 @@
 <script>
     import Gallery from '../comps/gallery.svelte'; 
 
-    let text = `The Milton Textures were developed by digitally altering photographs of reflections in water at night to invoke a wood-like feel.`
+    let text = `The Milton Textures were developed by digitally altering photographs of reflections in water at night to invoke a wood-like or flame-like feel.`
 
 </script>
 
