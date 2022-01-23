7c051c8939dd27fd74e481e18edb063930744771
Noah Syrkis <noah@syrkis.com>
2022-01-23 20:12:30 -0300

    miscellaneous release

---
+++ src/routes/miscellaneous.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Gallery from '../comps/gallery.svelte'; 
+
+    let text = `BCIIOI is a series of images of etched nano wires (potential uses include single neuron interfacing) captured through a microscope lens.`
+
+</script>
+
+<div>
+    <div>
+        <h1>
+            MISCELLANEOUS
+        </h1>
+    </div>
+    <Gallery project='Miscellaneous' {text} />
+</div>
