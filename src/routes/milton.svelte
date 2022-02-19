9f1f5ce58dd3f06d6361da65e04f81c9db7b240b
Noah Syrkis <noah@syrkis.com>
2022-02-19 10:13:49 +0100

    milton v3

--- src/routes/milton.svelte
+++ src/routes/milton.svelte
@@ -1,7 +1,7 @@
 <script>
     import Gallery from '../comps/gallery.svelte'; 
 
-    let text = `The Milton Textures were developed by using crop and exposure tools on photographs of quay lights reflected in water at night to achieve abstract motifs.`
+    let text = `The Milton Textures were developed by using crop and exposure tools on photos quay lights reflected at night to achieve abstract motifs.`
 
 </script>
 
