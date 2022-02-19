fae73a2595243fec8c1f8e7032803d91683754af
Noah Syrkis <noah@syrkis.com>
2022-02-19 10:14:04 +0100

    milton v3

--- src/comps/gallery.svelte
+++ src/comps/gallery.svelte
@@ -4,7 +4,7 @@
     import { onMount } from 'svelte';
 
 
-    let baseurl = "https://syrkis.ams3.cdn.digitaloceanspaces.com/noah"
+    let baseurl = "https://syrkis.ams3.digitaloceanspaces.com/noah"
     let data = "";
     let works = "";
 
