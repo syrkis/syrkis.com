4ca8d7a7d3ce09a6d215acdf0c118a8690f0a0b4
Noah Syrkis <noah@syrkis.com>
2022-11-09 16:11:32 +0100

    add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,30 @@
-<slot/>
+<script>
+    let links = [
+        ["instagram.com/noahsyrkis", "instagram"],
+        ["github.com/syrkis", "github"],
+        ["twitter.com/noahsyrkis", "twitter"],
+        ["youtube.com/channel/UCjhko_G0E9BT7-ustIvsEoA", "youtube"],
+        ["twitch.com/syrkis", "twitch"]
+    ];
+</script>
+
+<div>
+    <slot />
+    <div class='footer'>
+        <div>for all enquiries, write</div>
+        <div><a href="mailto:contact@syrkis.com">contact@syrkis.com</a></div>
+        <div class='social'>
+            {#each links as link}
+                <a href="https://{link[0]}" target="_blank" class="fab fa-{link[1]}" />
+            {/each}
+        </div>
+    </div>
+</div>
 
 <style>
+    .footer { text-align: center; line-height: 2em; padding-bottom: 10px; }
+    .fab { padding: 0 8px 0 8px; font-size: 0.8em; }
+    a { font-style: normal; font-weight: 100; }
 
     :global(body, html) {
         font-size: 1.1em;
