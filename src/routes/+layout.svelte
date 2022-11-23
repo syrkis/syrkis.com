fcd3893b70d164b3e2915117d64d156e3f3057cb
Noah Syrkis <noah@syrkis.om>
2022-11-23 05:11:33 -0500

    update gallery to allow for works that aren't for sale

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
         ["instagram.com/noahsyrkis", "instagram"],
         ["github.com/syrkis", "github"],
         ["twitter.com/noahsyrkis", "twitter"],
-        ["youtube.com/channel/UCjhko_G0E9BT7-ustIvsEoA", "youtube"],
+        ["youtube.com/@syrkis", "youtube"],
         ["twitch.com/syrkis", "twitch"]
     ];
 </script>
@@ -15,13 +15,14 @@
         <div><a href="mailto:contact@syrkis.com">contact@syrkis.com</a></div>
         <div class='social'>
             {#each links as link}
-                <a href="https://{link[0]}" target="_blank" class="fab fa-{link[1]}" />
+                <a href="https://{link[0]}" target="_blank" class="fab fa-{link[1]}"> </a>
             {/each}
         </div>
     </div>
 </div>
 
 <style>
+
     .footer { text-align: center; line-height: 2em; padding-bottom: 10px; }
     .fab { padding: 0 8px 0 8px; font-size: 0.8em; }
     a { font-style: normal; font-weight: 100; }
