199de29416509cb029f589a406002e9eb55583d1
Noah Syrkis <noah@syrkis.com>
2023-05-04 21:42:42 +0200

    description

--- src/routes/rhos/+page.svelte
+++ src/routes/rhos/+page.svelte
@@ -1,8 +1,8 @@
 <script>
     import Gallery from '../../comps/gallery.svelte';
 
-    let text = `Rhos is a computer generated collection of derivatives of manipulated Gaussian kernels,
-                exploring themes of growth, organicness, and light.`
+    let text = `Rhos is a computer generated collection of derivatives of manipulated Gaussian kernels.
+                The collection explores themes of growth and density.`
 
 </script>
 
