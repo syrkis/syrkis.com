8b176d52225c5abff4fc4b62ce42c9d35db0f044
Noah Syrkis <noah@syrkis.com>
2023-05-04 21:26:06 +0200

    add rhos

---
+++ src/routes/rhos/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Gallery from '../../comps/gallery.svelte';
+
+    let text = `Rhos is a computer generated collection of derivatives of manipulated Gaussian kernels,
+                exploring themes of growth, organicness, and light.`
+
+</script>
+
+<div>
+    <Gallery project='Rhos' {text} />
+</div>
