8d25522d26f9e8c4380e8fbdb75df832803bb12a
Noah Syrkis <noah@syrkis.com>
2024-03-18 11:37:11 -0300

    remove social links cos social media is stupid

--- src/comps/footer.svelte
+++ src/comps/footer.svelte
@@ -1,11 +1,11 @@
 <script>
-        let links = [
+/*        let links = [
         ["fab fa-github",                "https://github.com/syrkis"],
         ["fab fa-brands fa-orcid",       "https://orcid.org/0000-0002-6553-1115"],
         ["fab fa-youtube",               "https://youtube.com/@syrkis"],
         ["fab fa-brands fa-x-twitter ",  "https://x.com/noahsyrkis"],
         ["fas fa-regular fa-rss",          "https://virian.org/text"],
-    ];
+    ]; */
 </script>
 
 <div class='footer'>
