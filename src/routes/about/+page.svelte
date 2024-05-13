85e3a12d4b0e614cafcd058cebebfc986fc4b5fd
Noah Syrkis <noah@syrkis.com>
2024-05-13 06:37:20 -0300

    simplify about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,23 +1,11 @@
 <div class='wrapper'>
     <p>
-        We are a small team, led by Noah Syrkis, working in artificial life and intelligence, art,
-        information theory, and cognition. Our projects range from high resolution
-        photography capturing the ethereal moments of the natural world,
-        to complex computational models exploring the dynamics of artificial life and intelligence.
-    </p>    
-    <p>
-        Noah's background, rooted in neurocognition, computation and art positions our team uniquely
-        at the confluence of creativity and technology. We question and expand the creative boundaries of what is
-        understood about cognition, both organic and artificial (or both carbon and silicone based).
-    </p>
-    <p>
+        We are a small, art team, led by Noah Syrkis, exploring themes of cognition, self reference, and technology.
+        Our projects range from high resolution photography capturing the ethereal moments of the natural world,
+        to complex computational models exploring the dynamics of artificial life.
         Our projects range from: BCIIOI, exploring nano and brain tech; to RHOS, exploring notions of density and rotation;
         to the Erdős-Rényi illustrations, which plays with communication of science and mathematics;
         and to the Milton and Aurelian textures.
-    </p>
-    <p>
-        The work presented here are of a purely creative nature. All sales go to the furthering of the Virian Project.
-        You can support us by exploring our portfolio, buying our art, or supporting Virian directly.
     </p>
 </div>
 
