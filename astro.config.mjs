7380f9d7679e56d33514c7f034401cb18db11981
Noah Syrkis <noah@syrkis.com>
2024-12-29 00:53:29 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { typst } from "astro-typst";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [typst()],
+});
---
+++ src/pages/slides/miiii.typ
@@ -0,0 +1,15 @@
+#import "@preview/prequery:0.1.0"
+#import "@preview/touying:0.5.5": *
+#import themes.simple: *
+
+#show: simple-theme.with(aspect-ratio: "16-9")
+
+= Title
+
+== First Slide
+
+Hello, Touying!
+
+#pause
+
+Hello, Typst!
